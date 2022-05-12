865e3e41be88822a3f6d825b4338a0d447cddff8
pumpkin-ws <pumpkin_wang@foxmail.com>
2022-05-12 10:40:20 +0800

    update

--- examples/eih_calib.cpp
+++ examples/eih_calib.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv) {
     // eih.loadImages("./eih");
     // eih.loadCamParams("./camera_parameters.yml");
     // eih.loadPoses("./robot_poses.yaml");
+    eih.setResultDir("./");
     eih.calibrateHandEye("./eih", "./camera_parameters.yml", "./robot_poses.yaml", cv::Size(9, 6), 18, "mm", sparkvis::EIHCalibrator::Pattern::CHESSBOARD);
     std::cout << eih << std::endl;
 }
