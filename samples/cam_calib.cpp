42b3a11a95f106951ecb1eb5e3ad6c4cc9fada81
pumpkin-ws <pumpkin_wang@foxmail.com>
2022-05-14 07:52:35 +0800

    update with independent examples

---
+++ samples/cam_calib.cpp
@@ -0,0 +1,5 @@
+#include "sparkvis/camera_intrinsics.hpp"
+
+int main(int argc, char** argv) {
+    sparkvis::CamCalib cc;
+}
---
+++ samples/eih_calib.cpp
@@ -0,0 +1,5 @@
+#include "sparkvis/eye_in_hand.hpp"
+
+int main(int argc, char** argv) {
+    sparkvis::EIHCalibrator eih;
+}
